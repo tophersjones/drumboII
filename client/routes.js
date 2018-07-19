32b401643a1dbb2646134ac6b08048c2d94f5550
Chris <tophersjones@gmail.com>
2018-07-19 14:31:38 -0500

    commit

--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome} from './components'
+import {Login, Signup, UserHome, Snare} from './components'
 import {me} from './store'
 
 /**
@@ -19,12 +19,12 @@ class Routes extends Component {
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
-        <Route path="/login" component={Login} />
+        <Route path="/snare" component={Snare} />
         <Route path="/signup" component={Signup} />
+        <Route path="/" component={UserHome} />
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
-            <Route path="/home" component={UserHome} />
           </Switch>
         )}
         {/* Displays our Login component as a fallback */}
